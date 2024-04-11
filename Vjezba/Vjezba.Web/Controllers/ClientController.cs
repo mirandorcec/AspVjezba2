cab4d78fa367eacdf435172b952fd5aecfb03b5e
Miran Dorčec <mdorcec@tvz.hr>
2024-04-11 19:04:15 +0200

    drugi labos

---
+++ Vjezba/Vjezba.Web/Controllers/ClientController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Vjezba.Web.Mock;
+using Vjezba.Web.Models;
+
+namespace Vjezba.Web.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult Index(string? query)
+        {
+            if (!string.IsNullOrWhiteSpace(query))
+            {
+                var clients = MockClientRepository.Instance.All().Where(c => c.FullName.ToLower().Contains(query.ToLower())).ToList();
+                return View(clients);
+            }
+            else
+            {
+                var clients = MockClientRepository.Instance.All().ToList();
+                return View(clients);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Index(string queryName, string queryAddress)
+        {
+            var clients = MockClientRepository.Instance.All().ToList();
+            if (!string.IsNullOrWhiteSpace(queryName))
+            {
+                clients = clients.Where(c => c.FullName.ToLower().Contains(queryName.ToLower())).ToList();
+
+            }
+            if (!string.IsNullOrWhiteSpace(queryAddress))
+            {
+                clients = clients.Where(c => c.Address.ToLower().Contains(queryAddress.ToLower())).ToList();
+            }
+
+            return View(clients);
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (!id.HasValue)
+            {
+                ViewBag.ErrorMessage = "ID nije proslijeđen.";
+                return View("Error");
+            }
+            var client = MockClientRepository.Instance.FindByID(id.Value);
+            if (client == null)
+            {
+                ViewBag.ErrorMessage = $"Klijent s ID-om {id.Value} nije pronađen.";
+                return View("Error");
+            }
+            return View(client);
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public ActionResult AdvancedSearch(ClientFilterModel model)
+        {
+            var clients = MockClientRepository.Instance.All().ToList();
+            if (!string.IsNullOrWhiteSpace(model.FullName))
+            {
+                clients = clients.Where(c => c.FullName.ToLower().Contains(model.FullName.ToLower())).ToList();
+            }
+            if (!string.IsNullOrWhiteSpace(model.Email))
+            {
+                clients = clients.Where(c => c.Email.ToLower().Contains(model.Email.ToLower())).ToList();
+            }
+            if (!string.IsNullOrWhiteSpace(model.Address))
+            {
+                clients = clients.Where(c => c.Address.ToLower().Contains(model.Address.ToLower())).ToList();
+            }
+            if (!string.IsNullOrWhiteSpace(model.City))
+            {
+                clients = clients.Where(c => c.City != null && c.City.Name.ToLower().Contains(model.City.ToLower())).ToList();
+            }
+            return View("Index", clients);
+        }
+
+    }
+}
--- Vjezba/Vjezba.Web/Controllers/HomeController.cs
+++ Vjezba/Vjezba.Web/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using Vjezba.Web.Models;
 namespace Vjezba.Web.Controllers
 {
     public class HomeController(
-        ILogger<HomeController> _logger) 
+        ILogger<HomeController> _logger)
         : Controller
     {
         public IActionResult Index()
@@ -21,30 +21,24 @@ namespace Vjezba.Web.Controllers
         public IActionResult Contact()
         {
             ViewBag.Message = "Jednostavan način proslijeđivanja poruke iz Controller -> View.";
-            //Kao rezultat se pogled /Views/Home/Contact.cshtml renderira u "pravi" HTML
-            //Primjetiti - View() je poziv funkcije koja uzima cshtml template i pretvara ga u HTML
-            //Zasto bas Contact.cshtml? Jer se akcija zove Contact, te prema konvenciji se "po defaultu" uzima cshtml datoteka u folderu Views/CONTROLLER_NAME/AKCIJA.cshtml
-
 
             return View();
         }
 
-        /// <summary>
-        /// Ova akcija se poziva kada na formi za kontakt kliknemo "Submit"
-        /// URL ove akcije je /Home/SubmitQuery, uz POST zahtjev isključivo - ne može se napraviti GET zahtjev zbog [HttpPost] parametra
-        /// </summary>
-        /// <param name="formData"></param>
-        /// <returns></returns>
         [HttpPost]
         public IActionResult SubmitQuery(IFormCollection formData)
         {
-            //Ovdje je potrebno obraditi podatke i pospremiti finalni string u ViewBag
+            var ime = formData["ime"];
+            var prezime = formData["prezime"];
+            var email = formData["email"];
+            var poruka = formData["poruka"];
+            var tipPoruke = formData["tipPoruke"];
+            var newsletter = formData["newsletter"].Count > 0 ? "obavijestit ćemo vas" : "nećemo vas obavijestiti";
 
-            
+            var responsePoruka = $"Poštovani {ime} {prezime} ({email}) zaprimili smo vašu poruku te će vam se netko ubrzo javiti. Sadržaj vaše poruke je: {tipPoruke} - {poruka}. Također, {newsletter} o daljnjim promjenama preko newslettera.";
 
-            //Kao rezultat se pogled /Views/Home/ContactSuccess.cshtml renderira u "pravi" HTML
-            //Kao parametar se predaje naziv cshtml datoteke koju treba obraditi (ne koristi se default vrijednost)
-            //Trazenu cshtml datoteku je potrebno samostalno dodati u projekt
+            ViewBag.ResponsePoruka = responsePoruka;
+
             return View("ContactSuccess");
         }
 
@@ -52,6 +46,29 @@ namespace Vjezba.Web.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult FAQ(int? selected = null)
+        {
+            ViewBag.SelectedQuestion = selected;
+            
+            var faqs = new List<Tuple<string, string>>()
+            {
+                new("Tko bude prvak HNL-a?", "Dinamo"),
+                new("Tko bude prvak premier lige", "Liverpool"),
+                new("Tko će osvojiti ligu prvaka?", "Manchester City :("),
+                new("Tko je najbolji igrač svih vremena?", "The little boy from Rosario"),
+                new("Tko osvaja bundesligu?", "Leverkuseeeen")
+             };
+
+            ViewBag.FAQs = faqs;
+
+            return View();
+        }
+
+        public IActionResult ContactSuccess()
+        {
+            return View();
         }
     }
 }
